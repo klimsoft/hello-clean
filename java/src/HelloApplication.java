dabb233b6305eaad37e41cbebe1ac8ee092fe577
kamil.klimaszewski@gmail.com <kamil.klimaszewski@gmail.com>
2020-12-12 11:34:48 +0100

    First version

---
+++ java/src/HelloApplication.java
@@ -0,0 +1,9 @@
+import java.time.LocalDate;
+
+public class HelloApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World at " + LocalDate.now());
+	}
+
+}
---
+++ java/tests/HelloApplicationTests.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class HelloApplicationTests {
+
+	@Test
+	void sampleTest() {
+		int result = 2 + 2;
+		Assertions.assertEquals(4, 4);
+	}
+
+}
